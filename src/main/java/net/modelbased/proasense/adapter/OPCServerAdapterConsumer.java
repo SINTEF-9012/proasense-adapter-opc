091f934ae3fb3e8b9b5ddbfcffd19f4140162aa4
Brian Elvesæter <brian.elvesater@sintef.no>
2015-06-24 12:23:41 +0200

    Updated license headers.

--- src/main/java/net/modelbased/proasense/adapter/OPCServerAdapterConsumer.java
+++ src/main/java/net/modelbased/proasense/adapter/OPCServerAdapterConsumer.java
@@ -2,6 +2,7 @@
  * Copyright (C) 2014-2015 SINTEF
  *
  *     Brian Elvesæter <brian.elvesater@sintef.no>
+ *     Shahzad Karamat <shazad.karamat@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
